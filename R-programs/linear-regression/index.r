f7ce7215eacb348b05e24d71187bcdeadbe014fb
praxter11 <parveshsaini619@gmail.com>
2024-04-07 21:29:12 +0530

    test

--- R-programs/linear-regression/index.r
+++ R-programs/linear-regression/index.r
@@ -4,6 +4,7 @@ y <- 2*x + rnorm(100)
 data <- data.frame(x=x, y=y)
 model <- lm(y ~ x, data = data)
 summary(model)
-# visualizing
+
+
 plot(y ~ x, data = data, main = "Linear Regression", xlab = "X", ylab = "Y")
 abline(model, col = "blue")
